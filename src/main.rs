107e01b7af641b9d0ca4d53273d7045e088f042a
Wenyi Yu <wenyiyu@Wenyis-Laptop.local>
2025-12-26 13:06:08 +0800

    t

--- src/main.rs
+++ src/main.rs
@@ -71,10 +71,9 @@ abigen!(
     ]"#;
 
     // Aerodrome CL / Uniswap V3 Pool
-    IAerodromeCLPool,
+   IAerodromeCLPool,
     r#"[
         function slot0() external view returns (uint160 sqrtPriceX96, int24 tick, uint16, uint16, uint16, uint8, bool)
-        function token0() external view returns (address)
     ]"#
 );
 
